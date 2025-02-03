ddf28a85c33f8639b26ad2c99cb21c2233929ba2
4OH4Loop <semperchuck@gmail.com>
2025-02-03 18:40:32 -0500

    Initial Commit

---
+++ Predict/PredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredictApp.swift
+//  Predict
+//
+//  Created by Carolyn Ballinger on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
